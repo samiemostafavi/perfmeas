74accb52e6f9293f8a447e2c5722ae97b98c802c
Samie Mostafavi <ssmos@kth.se>
2023-07-14 13:48:25 +0200

    add init files

---
+++ ami.go
@@ -0,0 +1,33 @@
+package adv-mobile-info
+
+import (
+	"os"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func Run() {
+	http.HandleFunc("/", handleRequest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Define the JSON response
+	response := map[string]interface{}{
+		"message": "Hello, world!",
+	}
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Convert the response to JSON
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the JSON response
+	w.Write(jsonResponse)
+}
---
+++ cmd/ami/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+        "os"
+
+        ami "github.com/samiemostafavi/adv-mobile-info"
+)
+
+func main() {
+        ami.Run()
+}
